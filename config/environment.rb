7af4a8ac7d0f1e1b5a1736c24fdbeda977fba64f
John Gibbons <johngibbons10@gmail.com>
2015-01-04 21:03:46 -0700

    Ch. 13 complete - Added mailing for orders

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Email configuration
+Depot::Application.configure do 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+
+  }
+end
